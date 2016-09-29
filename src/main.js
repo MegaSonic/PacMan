8e34198fa5e47198a3a4e609586c1a33ec97b553
Adam Kay <u0988616@ad.utah.edu>
2016-09-28 20:33:48 -0600

    Added new coins, updated tilemap

--- src/main.js
+++ src/main.js
@@ -122,7 +122,7 @@ Pacman.prototype = {
 
         
         this.load.image('dot', 'assets/dot.png');
-        this.load.image('coin', 'assets/coin.png');
+        this.load.image('coin', 'assets/coin2.png');
 
         //this.load.tilemap('map', 'assets/pac_maze.json', null, Phaser.Tilemap.TILED_JSON);
         //this.load.image('tiles', 'assets/tile_set.png');
@@ -166,8 +166,8 @@ Pacman.prototype = {
         map.createFromTiles(21, 17, 'coin', secondLayer, this.dots);
 
         //  The dots will need to be offset by 12px to put them back in the middle of the grid
-        this.dots.setAll('x', 12, false, false, 1);
-        this.dots.setAll('y', 12, false, false, 1);
+        //this.dots.setAll('x', 12, false, false, 1);
+        //this.dots.setAll('y', 12, false, false, 1);
 
         this.spray = this.add.physicsGroup();
         map.createFromTiles(4, 4, 'dot', mapLayer, this.spray);
